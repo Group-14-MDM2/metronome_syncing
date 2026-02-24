1853df2792f0afd406997f87dc5553cff04df0b7
kokis1 <reuben.stannah@icloud.com>
2026-02-24 18:51:16 +0000

    updated the example model

--- kuramoto_model/simulation/first_example.py
+++ kuramoto_model/simulation/first_example.py
@@ -4,16 +4,19 @@ from kuramoto_simulation import Window, Screen_params, Model_params, Standard_St
 def generate_initial_angles(num_angles: int) -> list[float]:
    return np.linspace(0, 2*np.pi, num=num_angles).tolist()
 
+def generate_natural_frequencies(num_frequencies: int) -> list[float]:
+   return np.random.normal(0, 0.5, num_frequencies).tolist()
+
 
 def main() -> None:
-   N = 4
+   N = 8
    screen_params = Screen_params(width=800, 
                                  height=800, 
                                  radius=350, 
                                  background_colour=(0, 20, 80))
    
-   model_params = Model_params(K=0.8, 
-                               natural_frequencies=[0.2, 0.4, 0.5, 1], 
+   model_params = Model_params(K=1, 
+                               natural_frequencies=generate_natural_frequencies(N), 
                                initial_angles=generate_initial_angles(N), 
                                step_function=Standard_Step)
    simulation = Window(screen_params, 
