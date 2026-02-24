695e0fc3c5803598d3ead49baa2e1068aa4e2c84
kokis1 <reuben.stannah@icloud.com>
2026-02-24 16:16:31 +0000

    added more type hints to the example simulation

--- kuramoto_model/simulation/first_example.py
+++ kuramoto_model/simulation/first_example.py
@@ -1,7 +1,10 @@
 import numpy as np
 from kuramoto_simulation import Window, Screen_params, Model_params
 
-def step(t: float, Y: list[float], K: float, N: int, nat_freqs: list[float]) -> np.ndarray:
+def step(t: float, 
+         Y: list[float], 
+         K: float, N: int, 
+         nat_freqs: list[float]) -> np.ndarray:
    dYdt = []
    for i in range(N):
       d_theta_dt = nat_freqs[i] + K/N * sum(np.sin(Y[j] - Y[i]) for j in range(N))
@@ -9,9 +12,15 @@ def step(t: float, Y: list[float], K: float, N: int, nat_freqs: list[float]) -> 
    return np.array(dYdt)
 
 
-def main():
-   screen_params = Screen_params(800, 800, 350, (0, 20, 80))
-   model_params = Model_params(0.8, [0.2, 0.4, 0.5, 1], [0.6, 0.8, 0.1, 0], step)
+def main() -> None:
+   screen_params = Screen_params(width=800, 
+                                 height=800, 
+                                 radius=350, 
+                                 background_colour=(0, 20, 80))
+   model_params = Model_params(K=0.8, 
+                               natural_frequencies=[0.2, 0.4, 0.5, 1], 
+                               initial_angles=[0.6, 0.8, 0.1, 0], 
+                               step_function=step)
    simulation = Window(screen_params, 
                           model_params)
    simulation.main()
