616f5019ae0255d3bb32401be0e2bfbbd9ace165
cici <bm24110@bristol.ac.uk>
2026-04-02 18:04:54 +0100

    first.py

---
+++ mechanical_control/first.py
@@ -0,0 +1,143 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from dataclasses import dataclass
+from typing import Callable
+
+# setting the model parameters
+@dataclass
+class model_params:
+   n: int
+   m: float
+   M: float
+   lengths: np.ndarray
+   initial_theta: np.ndarray
+   g: float
+   epsilon: float
+
+params = model_params(4,
+                     0.25, 
+                     3, 
+                     np.array([0.1, 0.3, 0.1, 1]), 
+                     np.array([0.9, 0.25, 0.35, 0.71]), 
+                     9.81, 
+                     0.01)
+
+# making all the matricies
+
+def M(q: np.ndarray) -> np.ndarray:
+   m = np.zeros((params.n, params.n))
+
+   # the last row is different
+   for i in range(params.n - 1):
+      for j in range(params.n - 1):
+         if i == j:
+            m[i, j] = params.m * (params.lengths[i]**2)
+      m[i, -1] = params.m * params.lengths[i] * np.cos(q[i])
+   
+   for j in range(params.n - 1):
+      m[-1, j] = params.m * params.lengths[j]*np.cos(q[j])
+   m[-1, -1] = params.M + params.n * params.m
+   return m
+
+def C(q: np.ndarray, dqdt: np.ndarray) -> np.ndarray:
+   c = np.zeros((params.n, params.n))
+   
+   # the last row is different
+   for i in range(params.n - 1):
+      for j in range(params.n - 1):
+         if i == j:
+            c[i, j] = params.m * params.lengths[j]**2 * params.epsilon * ((q[j]/params.initial_theta[j])**2 - 1)
+   for j in range(params.n - 1):
+      c[-1, j] = params.m * params.lengths[j] * dqdt[j] * np.sin(q[j])
+   return c
+
+def G(q: np.ndarray) -> np.ndarray:
+   g = np.zeros(params.n)
+   for j in range(params.n - 1):
+      g[j] = params.m * params.g * params.lengths[j] * np.sin(q[j])
+   return g
+
+# def tau(q: np.ndarray, dqdt: np.ndarray) -> np.ndarray:
+#    t = np.zeros(params.n)
+#    return t
+
+
+# # the time-step function
+
+# def step(t: float, s: np.ndarray) -> np.ndarray:
+#    q = s[0, :]
+#    dqdt = s[1, :]
+#    dqqdtt = np.linalg.inv(M(q)) @ (tau(q, dqdt) - C(q, dqdt) @ dqdt - G(q))
+#    return np.vstack([dqdt, dqqdtt])
+
+# using RK4 to find the solution numerically
+def RK4(t_span: tuple[float, float], 
+        n: int, 
+        y_0: np.ndarray, 
+        step: Callable[[float, np.ndarray], np.ndarray]) -> tuple[list[np.ndarray], list[float]]:
+   t0 = t_span[0]
+   tf = t_span[1]
+   h = (tf - t0) / n
+   times = [t0]
+   Y = [y_0]
+   for _ in range(n):
+      t = times[-1]
+      y_k = Y[-1]
+      m1 = step(t, y_k)
+      m2 = step(t + h/2, y_k + m1 * h/2)
+      m3 = step(t + h/2, y_k + m2 * h/2)
+      m4 = step(t + h, y_k + m3 * h)
+      y_kp1 = y_k + h/6 * (m1 + 2*m2 + 2*m3 + m4)
+      Y.append(y_kp1)
+      times.append(t + h)
+   return Y, times
+#some data for the control model
+Kp = 50.0      
+Kd = 20.0      
+A = 0.05       
+omega = 5.7    
+
+def tau(t: float, q: np.ndarray, dqdt: np.ndarray) -> np.ndarray:
+   t_vec = np.zeros(params.n)
+   
+   # cart position
+   x = q[-1]
+   x_dot = dqdt[-1]
+   
+   x_d = A * np.sin(omega * t)
+   
+   error = x_d - x
+   cart_force = Kp * error - Kd * x_dot
+   
+   t_vec[-1] = cart_force
+   
+   return t_vec
+
+def step(t: float, s: np.ndarray) -> np.ndarray:
+   q = s[0, :]
+   dqdt = s[1, :]
+   dqqdtt = np.linalg.inv(M(q)) @ (tau(t, q, dqdt) - C(q, dqdt) @ dqdt - G(q))
+   return np.vstack([dqdt, dqqdtt])
+
+def main():
+   t0 = 0
+   tf = 15
+   n = 1000
+   y_0 = np.vstack([[0.9, 0.25, 0.35, 0],
+                    [0, 0, 0, 0]])
+   Y, times = RK4((t0, tf), n, y_0, step)
+
+   '''#plotting in the time domain
+   phases = [y[0, :-1] for y in Y]
+   plt.plot(times, phases)
+   plt.show()'''
+
+   # plotting in phase space
+   phases = [y[0, :-1] for y in Y]
+   velocities = [y[1, :-1] for y in Y]
+   plt.plot(phases, velocities)
+   plt.show()
+
+
+if __name__ == "__main__":
+   main()
